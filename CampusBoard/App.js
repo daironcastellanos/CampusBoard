bc6d5204cda891a71bdbe88f373453d62735c002
Dairon Castellanos <35010925+daironcastellanos@users.noreply.github.com>
2024-02-29 12:28:45 -0500

    mpv login and sign up fully functional with firebase

--- CampusBoard/App.js
+++ CampusBoard/App.js
@@ -1,3 +1,4 @@
+//App.js
 import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
